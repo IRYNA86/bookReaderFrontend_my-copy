96e1c0ca17a0506c8ee106a95db5a3377ca979e3
IRYNA86 <88017665+IRYNA86@users.noreply.github.com>
2022-09-25 01:07:04 +0300

    play with media

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,16 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+import Media from 'react-media';
 import Header from '../components/Header/Header.js'
 import InfoMobile from 'pages/info_mobile/InfoMobile.js';
-import { Route, Routes } from 'react-router-dom';
+import Register from 'pages/register/Register.js';
 
 export const App = () => {
   return (
     <div>
+       <Media queries={{
+          small: "(max-width: 599px)",
+          medium: "(min-width: 600px) and (max-width: 1199px)",
+          large: "(min-width: 1200px)"
+        }}>
+           {matches => (
+      <>
       <Header/>
       <Routes>
-      <Route path="/" element={<InfoMobile />} />
+      {matches.small && <Route path="/" element={<InfoMobile />} />}
       <Route path="/login" element={<p>Login</p>} />
-      <Route path="/register" element={<p>Register</p>} />
+      <Route path="/register" element={<Register/>} />
+      {matches.medium && <Route path="/" element={<Register/>} />}
       </Routes>
+      </>)}
+      </Media>
     </div>
   );
 };
